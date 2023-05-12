a70a88d7b0d1a5693a8cbd55f2d81438de2f3c58
ramatronics1 <shrihegde661@gmail.com>
2023-05-12 15:33:06 +0530

    login_page

--- App.js
+++ App.js
@@ -1,107 +1,71 @@
-import { useContext, useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import AppLoading from 'expo-app-loading';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Map from './screens/map';
+import Summary from './screens/summary';
+import Attendance from './screens/attendance';
+import CustomDrawer from './components/customdrawer';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import NewForm from './components/newform';
+import LoginPage from './screens/LoginScreen';
 
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import { Colors } from './constants/styles';
-import AuthContextProvider, { AuthContext } from './store/auth-context';
-import IconButton from './components/ui/IconButton';
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
-const Stack = createNativeStackNavigator();
-
-function AuthStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function AuthenticatedStack() {
-  const authCtx = useContext(AuthContext);
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: 'white',
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
-        options={{
-          headerRight: ({ tintColor }) => (
-            <IconButton
-              icon="exit"
-              color={tintColor}
-              size={24}
-              onPress={authCtx.logout}
-            />
-          ),
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
-
-function Navigation() {
-  const authCtx = useContext(AuthContext);
-
+export default function App() {
   return (
     <NavigationContainer>
-      {!authCtx.isAuthenticated && <AuthStack />}
-      {authCtx.isAuthenticated && <AuthenticatedStack />}
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Main" component={MainStack} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-function Root() {
-  const [isTryingLogin, setIsTryingLogin] = useState(true);
-
-  const authCtx = useContext(AuthContext);
-
-  useEffect(() => {
-    async function fetchToken() {
-      const storedToken = await AsyncStorage.getItem('token');
-
-      if (storedToken) {
-        authCtx.authenticate(storedToken);
-      }
-
-      setIsTryingLogin(false);
-    }
-
-    fetchToken();
-  }, []);
-
-  if (isTryingLogin) {
-    return <AppLoading />;
-  }
-
-  return <Navigation />;
-}
-
-export default function App() {
-  
+function MainStack() {
   return (
-    <>
-      <StatusBar style="light" />
-      <AuthContextProvider>
-        <Root />
-      </AuthContextProvider>
-    </>
-  );
+    <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />} useLegacyImplementation initialRouteName="Map">
+      <Drawer.Screen name="Map" component={Map} />
+      <Drawer.Screen name="Attendance" component={Attendance} />
+      <Drawer.Screen name="Summary" component={Summary} />
+      <Drawer.Screen name="NewForm" component={NewForm} />
+    </Drawer.Navigator>
+  );
 }
+
+function AddButton() {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    // Navigate to the new form screen
+    navigation.navigate('NewForm');
+  };
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={handlePress}>
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    right: 10,
+    top: 60,
+    backgroundColor: 'black',
+    width: 40,
+    height: 30,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,35 +1,79 @@
-import { useContext, useState } from 'react';
-import { Alert } from 'react-native';
+import React from 'react';
+import { TextInput, TouchableOpacity, Text, StyleSheet, View } from 'react-native';
 
-import AuthContent from '../components/Auth/AuthContent';
-import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { AuthContext } from '../store/auth-context';
-import { login } from '../util/auth';
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
 
-function LoginScreen() {
-  const [isAuthenticating, setIsAuthenticating] = useState(false);
-
-  const authCtx = useContext(AuthContext);
-
-  async function loginHandler({ email, password }) {
-    setIsAuthenticating(true);
-    try {
-      const token = await login(email, password);
-      authCtx.authenticate(token);
-    } catch (error) {
-      Alert.alert(
-        'Authentication failed!',
-        'Could not log you in. Please check your credentials or try again later!'
-      );
-      setIsAuthenticating(false);
+  handleLogin = () => {
+    const { username, password } = this.state;
+  
+    // Perform authentication check here
+    if (username === 'user' && password === 'password') {
+      // If authentication succeeds, navigate to the main app screen
+      // You can do this using navigation props or any other navigation library of your choice
+      console.log('Login successful');
+    } else {
+      // If authentication fails, display an error message to the user
+      console.log('Login failed');
     }
-  }
+  };
 
-  if (isAuthenticating) {
-    return <LoadingOverlay message="Logging you in..." />;
+  render() {
+    return (
+      <View style={styles.container}>
+           
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(text) => this.setState({ username: text })}
+          value={this.state.username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(text) => this.setState({ password: text })}
+          value={this.state.password}
+        />
+        <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+     
+      </View>
+     
+    );
   }
-
-  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
-export default LoginScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default LoginPage;
